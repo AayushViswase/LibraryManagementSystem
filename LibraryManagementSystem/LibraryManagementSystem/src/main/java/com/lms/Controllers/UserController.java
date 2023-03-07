afd333ba7154d6d5985192d2de8ad930dbfc00ca
AayushViswase <aayushviswase10@gmail.com>
2023-03-07 18:09:34 +0530

    added cross origins

--- LibraryManagementSystem/LibraryManagementSystem/src/main/java/com/lms/Controllers/UserController.java
+++ LibraryManagementSystem/LibraryManagementSystem/src/main/java/com/lms/Controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ import com.lms.Playload.Request.UserRequest;
 import com.lms.Playload.Response.UserResponse;
 import com.lms.Services.UserService;
 
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/lms/user")
 public class UserController {
